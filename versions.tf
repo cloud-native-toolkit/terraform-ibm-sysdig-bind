c4ee7c9c8b536855d4156281a6b6c17a9628c589
Sean Sundberg <seansund@us.ibm.com>
2021-04-03 22:59:38 -0500

    Pins ibm provider version at 1.22.0
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     ibm = {
       source = "ibm-cloud/ibm"
-      version = ">= 1.9.0"
+      version = "1.22.0"
     }
     helm = {
       version = ">= 1.1.1"
     }
   }
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
 }
