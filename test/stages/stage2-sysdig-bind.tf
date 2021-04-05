dfa5d2c4e94ed077b04cd320bc07f29442bad8b8
Sean Sundberg <seansund@us.ibm.com>
2021-04-04 21:33:53 -0500

    Adds ibmcloud_api_key to test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-sysdig-bind.tf
+++ test/stages/stage2-sysdig-bind.tf
@@ -1,8 +1,9 @@
 module "sysdig-bind" {
   source = "./module"
 
-  resource_group_name      = module.resource_group.name
   region                   = var.region
+  ibmcloud_api_key         = var.ibmcloud_api_key
+  resource_group_name      = module.resource_group.name
   guid                     = module.dev_sysdig.guid
   access_key               = module.dev_sysdig.access_key
   cluster_name             = module.dev_cluster.name
