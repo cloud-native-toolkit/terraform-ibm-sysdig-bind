817f4ad52a435f7d7732601dbcd669b08fffbf80
Sean Sundberg <seansund@us.ibm.com>
2021-04-19 23:43:00 -0500

    Adds ocp-vpc to module dependencies
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,14 +1,16 @@
 module "dev_cluster" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
 
-  resource_group_name     = var.resource_group_name
-  region                  = var.region
-  ibmcloud_api_key        = var.ibmcloud_api_key
-  name                    = var.cluster_name
-  worker_count            = 2
-  name_prefix             = var.name_prefix
-  exists                  = true
-  cos_id                  = ""
-  vpc_subnet_count        = 1
-  vpc_name                = ""
+  resource_group_name = module.resource_group.name
+  region              = var.region
+  ibmcloud_api_key    = var.ibmcloud_api_key
+  name                = var.cluster_name
+  worker_count        = var.worker_count
+  ocp_version         = var.ocp_version
+  exists              = var.cluster_exists
+  name_prefix         = var.name_prefix
+  vpc_name            = module.vpc.name
+  vpc_subnets         = module.subnets.subnets
+  vpc_subnet_count    = module.subnets.count
+  cos_id              = module.cos.id
 }
