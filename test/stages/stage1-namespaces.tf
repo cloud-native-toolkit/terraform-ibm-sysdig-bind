0bfec7a1aad2234335d1dcf02fc5ef97b84c86d7
Sean Sundberg <seansund@us.ibm.com>
2021-04-20 00:26:06 -0500

    Adds ocp-vpc to module dependencies (#2)
    * Adds ocp-vpc to module dependencies
    * Updates test variables
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-namespaces.tf
+++ test/stages/stage1-namespaces.tf
@@ -1,5 +1,5 @@
 module "dev_tools_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
+  source = "github.com/ibm-garage-cloud/terraform-cluster-namespace.git"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
   name                     = var.namespace
